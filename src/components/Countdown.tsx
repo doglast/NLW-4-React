8ec37c0de37002b24f80491a5cfc90b32997165a
Doglast <des212425@gmail.com>
2021-02-24 13:26:49 -0300

    fim da aula 2

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,18 +1,54 @@
+import { useState, useEffect } from 'react';
 import styles from '../styles/components/Countdown.module.css';
 
 export function Countdown() {
+
+  const [time, setTime] = useState(17 * 60);
+  const [active, setActive] = useState(false);
+
+  const minutes = Math.floor(time / 60);
+  const seconds = (time % 60);
+
+  const [minuteLeft, minuteRigth] = String(minutes).padStart(2, '0').split('');
+  const [secondLeft, secondRigth] = String(seconds ).padStart(2, '0').split('');
+
+  function startCountdown(){
+    setActive(true);
+  }
+
+  useEffect(() => {
+    if (active && time > 0){
+      setTimeout(() => {
+        setTime(time-1);
+      }, 1000)
+    }
+  }, [active, time])
+
   return (
-    <div className={styles.countdownContainer}>
-      <div>
-        <span>1</span>
-        <span>3</span>
-      </div>
-      <span>:</span>
-      <div>
-        <span>4</span>
-        <span>9</span>
+    <div>
+      
+      {/* Timer */}
+      <div className={styles.countdownContainer}>
+        <div>
+          <span>{minuteLeft}</span>
+          <span>{minuteRigth}</span>
+        </div>
+        <span>:</span>
+        <div>
+          <span>{secondLeft}</span>
+          <span>{secondRigth}</span>
+        </div>
       </div>
 
+      {/* Botão */}
+
+      <button
+        type='button' 
+        className={styles.countdownButton}
+        onClick={startCountdown}
+      >
+        É hora de codar
+      </button>
     </div>
   );
 }
